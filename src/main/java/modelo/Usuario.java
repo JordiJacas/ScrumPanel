ba136e9d8b5e265156f62a3ae82390dc05be4a1e
JordiJacas <jjacasventura@iesesteveterradas.cat>
2019-01-28 17:39:33 +0100

    AutoIncremental - Usuario.java & Proyecto.java (GenerationType.IDENTITY)

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -6,6 +6,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -18,7 +19,7 @@ import enumClass.userTypeEnum;
 public class Usuario {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int usuario_id;
 	
 	@Column(unique = true, nullable = false)
