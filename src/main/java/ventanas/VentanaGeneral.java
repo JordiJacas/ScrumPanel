7f45b4a3c2d3404d680cdcaa5bba597532ba2339
JordiJacas <jjacasventura@iesesteveterradas.cat>
2019-02-01 18:35:46 +0100

    Sprint 2

--- src/main/java/ventanas/VentanaGeneral.java
+++ src/main/java/ventanas/VentanaGeneral.java
@@ -5,6 +5,7 @@ import java.awt.Panel;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.beans.PropertyVetoException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
@@ -51,8 +52,10 @@ public class VentanaGeneral extends JFrame {
 	private JMenuItem sBuscarProyecto;
 	private JPanel pNombreSalir;
 	private JLabel lblUsuario;
-	private ArrayList<JInternalFrame> internalFrames;
+	private ArrayList<JMenuItem> menuItems;
 	private JButton btnSalir;
+	private JPanel login;
+	private JPanel nuevoUsuario;
 	
 	/**
 	 * Launch the application.
@@ -76,107 +79,89 @@ public class VentanaGeneral extends JFrame {
 	    setTitle("SCRUM - " + state);
 	    setSize(800,500);
 	    setVisible(true);
-	    internalFrames = new ArrayList<JInternalFrame>();
+	    
+	    menuItems = new ArrayList<JMenuItem>();
 	    
 		dp = new JDesktopPane();
 		dp.setBackground(Color.WHITE);
 		getContentPane().add(dp);
 		
+		addVentanaLogin();
+		
 		menuBar = new JMenuBar();
 		menuBar.setBounds(0, 0, 782, 26);
 		dp.add(menuBar);
+		menuBar.setVisible(false);
 		
 		mUsuarios = new JMenu("Usuarios");
 		menuBar.add(mUsuarios);
 		
 		sNuevoUsuario = new JMenuItem("Nuevo Usuario");
+		menuItems.add(sNuevoUsuario);
 		sNuevoUsuario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				nuevoUsuario = new VentanaUsuarios();
+				iNuevoUsuario = new JInternalFrame("Nuevo Usuario");
+				 
+				iNuevoUsuario.setLocation(100, 107);
+				
+				iNuevoUsuario.getContentPane().add(nuevoUsuario);
+
+				iNuevoUsuario.pack();
+				
+				iNuevoUsuario.setResizable(true);
+				iNuevoUsuario.setClosable(true);
+				
 				iNuevoUsuario.setVisible(true);
+				
+				dp.add(iNuevoUsuario);
+				
 			}
 		});
 		mUsuarios.add(sNuevoUsuario);
 		
 		sBuscarmodificarUsuario = new JMenuItem("Buscar/Modificar Usuario");
+		menuItems.add(sBuscarmodificarUsuario);
 		sBuscarmodificarUsuario.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				iNuevoUsuario.setVisible(true);
 			}
 		});
 		mUsuarios.add(sBuscarmodificarUsuario);
+		
 		mProyectos = new JMenu("Proyectos");
 		menuBar.add(mProyectos);
 		
 		sCrearProyecto = new JMenuItem("Crear Proyecto");
+		menuItems.add(sCrearProyecto);
 		sCrearProyecto.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
 				JPanel nuevoProyecto = new VentanaProyecto();
 				iNuevoProyecto = new JInternalFrame("Nuevo Proyecto");
 				iNuevoProyecto.setLocation(89, 71);
-				internalFrames.add(iNuevoProyecto);
 				iNuevoProyecto.getContentPane().add(nuevoProyecto);
 				iNuevoProyecto.pack();
 				iNuevoProyecto.setResizable(true);
 				iNuevoProyecto.setClosable(true);
-				dp.add(iNuevoProyecto);
 				iNuevoProyecto.setVisible(true);
+				dp.add(iNuevoProyecto);
+
 			}
 		});
 		mProyectos.add(sCrearProyecto);
 		
 		JMenuItem sBuscarProyecto = new JMenuItem("Buscar Proyectos");
+		menuItems.add(sBuscarProyecto);
 		sBuscarProyecto.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				iNuevoProyecto.setVisible(true);
 			}
 		});
 		mProyectos.add(sBuscarProyecto);
 		
-		mUsuarios.setVisible(false);
-		mProyectos.setVisible(false);
-	
-		// Se construye el JInternalFrame
-		iLogin = new JInternalFrame("Login");
-		iLogin.setSize(156, 94);
-		iLogin.setLocation(133, 58);
-		internalFrames.add(iLogin);
-		
-		iNuevoUsuario = new JInternalFrame("Nuevo Usuairo");
-		iNuevoUsuario.setLocation(28, 107);
-		internalFrames.add(iNuevoUsuario);
-		
-		//Creamos el panel que contendra el JInternalFrame
-		JPanel login = new VentanaLogin(this, iLogin);
-		JPanel nuevoUsuario = new VentanaUsuarios();
-		
-		//Añadimos el panel al JInternalFram
-		iLogin.getContentPane().add(login);
-		iNuevoUsuario.getContentPane().add(nuevoUsuario);
-		
-		
-		// Es importante darle tamaño -pack()- al JInternalFrame,
-		// porque si no, tendrá tamaño 0,0 y no lo veremos.
-		iLogin.pack();
-		iNuevoUsuario.pack();
-		
-		
-		// Por defecto el JInternalFrame no es redimensionable ni
-		// tiene el botón de cerrar, así que se lo ponemos.
-		iLogin.setResizable(true);
-		iLogin.setClosable(true);
-		iNuevoUsuario.setResizable(true);
-		iNuevoUsuario.setClosable(true);
-		
-		
-		iLogin.setDefaultCloseOperation(HIDE_ON_CLOSE);
-		iNuevoUsuario.setDefaultCloseOperation(HIDE_ON_CLOSE);
-		//iNuevoProyecto.setDefaultCloseOperation(HIDE_ON_CLOSE);
-		
-		// Se mete el internal en el JDesktopPane
-		dp.add(iLogin);
-		dp.add(iNuevoUsuario);
-		
+		sBuscarProyecto.setEnabled(false);
+		sCrearProyecto.setEnabled(false);
+		sBuscarmodificarUsuario.setEnabled(false);
+		sNuevoUsuario.setEnabled(false);		
 		
 		pNombreSalir = new JPanel();
 		pNombreSalir.setBounds(0, 26, 782, 26);
@@ -188,15 +173,15 @@ public class VentanaGeneral extends JFrame {
 		btnSalir = new JButton("Salir");
 		btnSalir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
-				
-				for(JInternalFrame internalFrame: internalFrames) {
-					internalFrame.setVisible(false);
-				}
-				
 				btnSalir.setVisible(false);
 				lblUsuario.setText("");
-				iLogin.setVisible(true);
+				menuBar.setVisible(false);
+				for(JMenuItem menuItem: menuItems) {
+					if(menuItem.isEnabled()) {
+						menuItem.setEnabled(false);
+					}
+				}
+				addVentanaLogin();
 			}
 		});
 		GroupLayout gl_panel = new GroupLayout(pNombreSalir);
@@ -215,28 +200,55 @@ public class VentanaGeneral extends JFrame {
 					.addComponent(lblUsuario, GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE)
 					.addComponent(btnSalir))
 		);
+		
 		pNombreSalir.setLayout(gl_panel);
 		btnSalir.setVisible(false);
 		
-		// Se visualiza el JInternalFrame 
-		iLogin.setVisible(true);
-		iNuevoUsuario.setVisible(false);
 	}
 	
 	public void visible(Usuario usuario) {
 		
-		iLogin.setVisible(false);
-		if(usuario.getRol_usuario().equals(userTypeEnum.USER_ADMINISTRATOR)){
-			mUsuarios.setVisible(true);
+		try {
+			iLogin.setClosed(true);
+		} catch (PropertyVetoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		mProyectos.setVisible(true);
+		menuBar.setVisible(true);
 		btnSalir.setVisible(true);
-		pNombreSalir.updateUI();
-		
 		lblUsuario.setText("Usuario: " + UsuarioConectado.getNombreUsuario() + " (" + UsuarioConectado.getRolUsuario() + ")");
 		
+		if(usuario.getRol_usuario().equals(userTypeEnum.USER_ADMINISTRATOR)){
+			sNuevoUsuario.setEnabled(true);
+			sBuscarmodificarUsuario.setEnabled(true);
+		}
 		
+		if(usuario.getRol_usuario().equals(userTypeEnum.DEVELOPER)) {
+			
+		}
+		
+		if(usuario.getRol_usuario().equals(userTypeEnum.PRODUCT_OWNER)){
+			
+		}
+		
+		if(usuario.getRol_usuario().equals(userTypeEnum.SCRUM_MASTER)){
+			sCrearProyecto.setEnabled(true);
+			sBuscarProyecto.setEnabled(true);
+		}		
+	}
+	
+	public void addVentanaLogin() {
+		login = new VentanaLogin(this, iLogin);
+		iLogin = new JInternalFrame("Login");
+		iLogin.setSize(156, 94);
+		iLogin.setLocation(133, 58);
+		iLogin.getContentPane().add(login);
+		iLogin.pack();
+		iLogin.setResizable(true);
+		iLogin.setClosable(true);
+		iLogin.setVisible(true);
+		iLogin.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		dp.add(iLogin);
 		
 	}
 }
